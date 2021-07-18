566c73808eb42fc5269f4412aca4e1c592493798
yutangtai <dajiu0224@gmail.com>
2021-07-18 14:13:34 +0800

    size => sizes

--- vue.config.js
+++ vue.config.js
@@ -12,74 +12,74 @@ module.exports = {
       icons: [
         {
           src: './img/icons/android-chrome-192x192.png',
-          size: '192x192',
+          sizes: '192x192',
           type: 'image/png'
         },
         {
           src: './img/icons/android-chrome-512x512.png',
-          size: '512x512',
+          sizes: '512x512',
           type: 'image/png'
         },
         {
           src: './img/icons/android-chrome-maskable-192x192.png',
-          size: '192x192',
+          sizes: '192x192',
           type: 'image/png',
           purpose: 'maskable'
         },
         {
           src: './img/icons/android-chrome-maskable-512x512.png',
-          size: '512x512',
+          sizes: '512x512',
           type: 'image/png',
           purpose: 'maskable'
         },
         {
           src: './img/icons/apple-touch-icon-60x60.png',
-          size: '60x60',
+          sizes: '60x60',
           type: 'image/png'
         },
         {
           src: './img/icons/apple-touch-icon-76x76.png',
-          size: '76x76',
+          sizes: '76x76',
           type: 'image/png'
         },
         {
           src: './img/icons/apple-touch-icon-120x120.png',
-          size: '120x120',
+          sizes: '120x120',
           type: 'image/png'
         },
         {
           src: './img/icons/apple-touch-icon-152x152.png',
-          size: '152x152',
+          sizes: '152x152',
           type: 'image/png'
         },
         {
           src: './img/icons/apple-touch-icon-180x180.png',
-          size: '180x180',
+          sizes: '180x180',
           type: 'image/png'
         },
         {
           src: './img/icons/apple-touch-icon.png',
-          size: '180x180',
+          sizes: '180x180',
           type: 'image/png'
         },
         {
           src: './img/icons/favicon-16x16.png',
-          size: '16x16',
+          sizes: '16x16',
           type: 'image/png'
         },
         {
           src: './img/icons/favicon-32x32.png',
-          size: '32x32',
+          sizes: '32x32',
           type: 'image/png'
         },
         {
           src: './img/icons/msapplication-icon-144x144.png',
-          size: '144x144',
+          sizes: '144x144',
           type: 'image/png'
         },
         {
           src: './img/icons/mstile-150x150.png',
-          size: '150x150',
+          sizes: '150x150',
           type: 'image/png'
         }
       ]
